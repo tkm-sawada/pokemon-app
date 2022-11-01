99a1033878b1127e313bc00cc3d3575805f2e632
tkm-sawada <da.tkm3@gmail.com>
2022-11-01 22:45:00 +0900

    20221101_2_pageButtonFix

--- src/components/PageButton.tsx
+++ src/components/PageButton.tsx
@@ -16,9 +16,9 @@ const PageButton = memo((props: Props) => {
         {pages.map((pageNum: number, i: number) => {
           return (
             pageNum === nowPage ? (
-              <button key={i} className="flex-1 md:mx-4 mx-1 py-3 md:px-4 px-2 text-white bg-gray-800 border-0 hover:bg-gray-500 rounded-xl text-xl shadow-2xl pointer-events-none" disabled>{pageNum}</button>
+              <button key={i} className="flex-1 md:mx-4 mx-1 py-3 md:px-4 px-1 text-white bg-gray-800 border-0 hover:bg-gray-500 rounded-xl text-xl shadow-2xl pointer-events-none" disabled>{pageNum}</button>
             ) : (
-              <button key={i} onClick={() => handleMovePage({pageNum})} className="flex-1 md:mx-4 mx-1 py-3 md:px-4 px-2 text-black bg-white border-0 hover:bg-gray-200 rounded-xl text-xl shadow-2xl">{pageNum}</button>
+              <button key={i} onClick={() => handleMovePage({pageNum})} className="flex-1 md:mx-4 mx-1 py-3 md:px-4 px-1 text-black bg-white border-0 hover:bg-gray-200 rounded-xl text-xl shadow-2xl">{pageNum}</button>
             )
           )
         })}

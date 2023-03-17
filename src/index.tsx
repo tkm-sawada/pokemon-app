56aa4b11e3b9243caa143f4f8c238c17d9a04ead
tkm-sawada <da.tkm3@gmail.com>
2023-03-17 09:34:17 +0900

    20230317_commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { NowPageProvider } from './components/providers/NowPageProvider';
-import { PokemonDataProvider } from './components/providers/PokemonDataProvider';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { NowPageProvider } from './components/providers/NowPageProvider'
+import { PokemonDataProvider } from './components/providers/PokemonDataProvider'
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   //<React.StrictMode>
-    <NowPageProvider>
-      <PokemonDataProvider>
-        <App />
-      </PokemonDataProvider>
-    </NowPageProvider>
+  <NowPageProvider>
+    <PokemonDataProvider>
+      <App />
+    </PokemonDataProvider>
+  </NowPageProvider>
   //</React.StrictMode>
-);
+)
